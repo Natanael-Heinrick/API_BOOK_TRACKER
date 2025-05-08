6d358c937732f6563725376b82e1db62a1138050
Natanael Heinrick <natanaelheinrick12@gmail.com>
2025-05-07 22:56:53 -0300

    fix(connection): fixed export of MongoDB connection function

--- db/connect.js
+++ db/connect.js
@@ -3,13 +3,18 @@ require('dotenv').config();
 
 const mongoURI = process.env.MONGO_URI;
 
-mongoose.connect(mongoURI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(() => {
-    console.log('Connected to MongoDB successfully!');
-}).catch((error) => {   
-    console.error('Error connecting to MongoDB:', error);
-});
+const connect = async () =>{
+    try{
+        await mongoose.connect(mongoURI, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log('MongoDB connected successfully');
+    } catch(e){
+        console.log('Err connecting to MongoDB', e);
+        throw e;
+    }
+}
 
-module.exports = mongoose;
+
+module.exports = connect;
--- index.js
+++ index.js
@@ -5,4 +5,11 @@ const mongoose = require('mongoose');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.listen(3000, () => console.log('Server is running on port 3000'));
+const connect = require('./db/connect');
+
+connect().then(() => {
+    app.listen(3000, () => console.log('Server is running on port 3000'));
+}).catch((error) => {
+    console.error('Error connecting to the database:', error);
+});
+
