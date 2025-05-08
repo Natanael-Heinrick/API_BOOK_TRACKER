e5b89748660b328bb8b1152fe5cea7dfdc9e01c7
Natanael Heinrick <natanaelheinrick12@gmail.com>
2025-05-07 22:37:23 -0300

    chore(config): add .env for MongoDB connection

---
+++ db/connect.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+const mongoURI = process.env.MONGO_URI;
+
+mongoose.connect(mongoURI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB successfully!');
+}).catch((error) => {   
+    console.error('Error connecting to MongoDB:', error);
+});
+
