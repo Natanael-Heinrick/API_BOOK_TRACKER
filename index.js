5ef8dc24f4157fafa43ab38353364e50a529f991
Natanael Heinrick <natanaelheinrick12@gmail.com>
2025-05-08 23:52:14 -0300

    feat(index): adding routes inside  the file with message write welcome

--- index.js
+++ index.js
@@ -7,6 +7,14 @@ app.use(express.urlencoded({ extended: true }));
 
 const connect = require('./db/connect');
 
+const bookRoutes = require('./routes/bookRoutes');
+app.use('/api', bookRoutes);
+
+
+app.get('/', (req,res) => {
+    res.json({ message: 'Welcome to the Book API :), to use the API, please use /api/books' });
+})
+
 connect().then(() => {
     app.listen(3000, () => console.log('Server is running on port 3000'));
 }).catch((error) => {
